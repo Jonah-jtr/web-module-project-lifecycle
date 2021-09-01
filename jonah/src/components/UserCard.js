d069c0a7b9dfbc7af12a374a23a77bc25f185aa4
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-08-31 23:41:31 -0500

    structure set

--- jonah/src/components/UserCard.js
+++ jonah/src/components/UserCard.js
@@ -7,7 +7,17 @@ class UserCard extends React.Component {
         <div className="userImages">
           <img src={this.props.user.avatar_url} alt="user avatar" />
         </div>
-        <h1> User: {this.props.user.login}</h1>
+
+        <div className="userInfo">
+          <h1>{this.props.user.name}</h1>
+          <h2> User: {this.props.user.login}</h2>
+          <h4>{this.props.user.location}.</h4>
+          <h4>Followers: {this.props.user.followers}</h4>
+          <h4>Following: {this.props.user.following}</h4>
+          <a href={this.props.user.html_url} target="_blank">
+            Github
+          </a>
+        </div>
       </div>
     );
   }
